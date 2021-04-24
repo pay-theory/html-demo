007359da8aa01685b29e7915172883d2b7850815
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-04-24 19:56:12 +0000

    initial

--- shell/model.formation.sh
+++ shell/model.formation.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 ENV=""
+API_KEY=""
 
 aws cloudformation create-stack \
 --region 'us-east-1' \
 --stack-name html-example-${ENV} \
 --template-body file://formation.yml \
---parameters ParameterKey=Env,ParameterValue=${ENV}
+--parameters ParameterKey=Env,ParameterValue=${ENV} \
+    ParameterKey=ApiKey,ParameterValue=${API_KEY}
