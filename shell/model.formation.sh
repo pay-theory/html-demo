d804ab153eda361c66404ba30685a479ee0c1d14
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-04-27 13:03:59 +0000

    formation fix

--- shell/model.formation.sh
+++ shell/model.formation.sh
@@ -6,6 +6,6 @@ API_KEY=""
 aws cloudformation create-stack \
 --region 'us-east-1' \
 --stack-name html-example-${ENV} \
---template-body file://formation.yml \
+--template-body file://formation.${ENV}.yml \
 --parameters ParameterKey=Env,ParameterValue=${ENV} \
     ParameterKey=ApiKey,ParameterValue=${API_KEY}
