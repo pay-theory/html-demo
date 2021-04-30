e4bc166f497b7769f913976c28addb525c6a84c4
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-04-30 14:26:42 +0000

    new build

--- shell/build_env.sh
+++ shell/build_env.sh
@@ -1,4 +1,5 @@
 export ENVIRONMENT=$1
+export SDK_URL="https://dev.sdk.paytheorystudy.com/aron/index.js"
 mkdir public/html/$ENVIRONMENT
 echo $ENVIRONMENT Credit Card Build started on `date`
 sed 's#TEMPLATE_URL#'$SDK_URL'#g' templates/pay-theory-credit-card.html | sed 's/TEMPLATE_ENVIRONMENT/'$ENVIRONMENT'/g' > public/html/$ENVIRONMENT/pay-theory-credit-card.html
