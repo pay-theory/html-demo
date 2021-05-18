d70be3199919b22d8ff353b8f07f239aaeaa6767
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-05-18 14:42:53 +0000

    adding account code and reference tags

--- shell/build_env.sh
+++ shell/build_env.sh
@@ -2,6 +2,7 @@ export PARTNER=$1
 export STAGE=$2
 export ENVIRONMENT=$1-$2
 export SDK_URL="https://$PARTNER.sdk.$STAGE.com/index.js"
+rm -rf public/html/$PARTNER-$STAGE
 mkdir public/html/$PARTNER-$STAGE
 echo $PARTNER-$STAGE Credit Card Build started on `date`
 sed 's#TEMPLATE_URL#'$SDK_URL'#g' templates/pay-theory-credit-card.html | sed 's/TEMPLATE_ENVIRONMENT/'$ENVIRONMENT'/g' > public/html/$ENVIRONMENT/pay-theory-credit-card.html
