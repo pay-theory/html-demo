e28ad5e6b2ae23f87072e779b5bb77c21542a213
aron@paytheory.com <ubuntu@ip-172-31-37-209.ec2.internal>
2021-06-05 18:38:34 +0000

    moved deployment to end

--- shell/create_new_partner_stage.sh
+++ shell/create_new_partner_stage.sh
@@ -3,8 +3,7 @@
 PARTNER=$1
 STAGE=$2
 
-git checkout -b $PARTNER-$STAGE
-git push --set-upstream origin $PARTNER-$STAGE
+git push origin main:$PARTNER-$STAGE
 
 sh shell/formation.sh $PARTNER $STAGE create
 sh shell/build_env.sh $PARTNER $STAGE
--- shell/formation.sh
+++ shell/formation.sh
@@ -5,6 +5,7 @@ STAGE=$2
 MODE=$3
 aws cloudformation ${MODE}-stack \
 --region 'us-east-1' \
+--capabilities CAPABILITY_IAM \
 --stack-name html-example-${PARTNER}-${STAGE} \
 --template-body file://formation.yml \
 --parameters ParameterKey=Partner,ParameterValue=${PARTNER} \
