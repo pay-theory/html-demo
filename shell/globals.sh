38e75aa97388cbe65b1845200a60d43b7d459ec0
EC2 Default User <ec2-user@ip-172-31-24-77.ec2.internal>
2021-06-30 12:23:47 +0000

    formation refactor

--- shell/globals.sh
+++ shell/globals.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 MODE=$1
+PARTNER=$2
 REPO=html-demo
 aws cloudformation ${MODE}-stack \
 --region 'us-east-1' \
 --capabilities CAPABILITY_NAMED_IAM \
 --stack-name pt-account-$REPO \
---template-body file://global.yml
+--template-body file://global.yml --parameters ParameterKey=Partner,ParameterValue=${PARTNER}
