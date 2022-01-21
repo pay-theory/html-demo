0b394665c9b875beb7a180d6ade2d2b1aaaf8eb7
Abel Collins <66232145+abelcollins@users.noreply.github.com>
2022-01-21 12:19:10 -0500

    fix 01_prepare script

--- shell/01_prepare.sh
+++ shell/01_prepare.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
-export PARTNER=$1
-export STAGE=$2
+PARTNER=$1
+STAGE=$2
+SERVICE_TYPE=$3
+SERVICE_NAME=$4
+MODE=$6
 
-existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE})
+# if there is a .git directory we are in partner-factory
+if [ -d ".git" ]
+then
+
+# if we do not have the branch in remote create it
+existed_in_remote=$(git ls-remote --heads origin ${PARTNER}${MODE}-${STAGE} 2>/dev/null)
 if [[ -z ${existed_in_remote} ]]
 then
     echo "Branch ${PARTNER}${MODE}-${STAGE} not existed in remote, Creating now..."
     git checkout -b ${PARTNER}${MODE}-${STAGE}
     git push -u origin ${PARTNER}${MODE}-${STAGE}
+
+#end if we do not have the branch in remote create it
 fi
 
+#end if there is a .git directory we are in partner-factory
+fi
 export ENVIRONMENT=$1-$2
 export SDK_URL="https://$PARTNER.sdk.$STAGE.com/index.js"
 
