2bc2fac90bf511a86c68efb39c157c808ac1f5f6
Abel Collins <66232145+abelcollins@users.noreply.github.com>
2022-08-17 09:54:36 -0400

    codacy fixes and code cleanup

--- shell/02_deploy.sh
+++ shell/02_deploy.sh
@@ -16,13 +16,15 @@ echo "STAGE :: $STAGE"
 echo "SERVICE_NAME :: $SERVICE_NAME"
 echo "SERVICE_TYPE :: $SERVICE_TYPE"
 echo "TARGET_MODE :: $TARGET_MODE"
+echo "S3_ARTIFACTS_BUCKET :: $S3_ARTIFACTS_BUCKET"
+echo "S3_ARTIFACTS_PATH :: $S3_ARTIFACTS_PATH"
 
 
 echo "Validating the cfn templates $(date) in $(pwd)" ;
 sam validate -t ./templates/formation.yml ;
 echo "Starting SAM build $(date) in $(pwd)" ;
 
-aws s3 cp public s3://html-demo-$TARGET_ACCOUNT_ID-${PARTNER}-${STAGE} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+aws s3 cp public s3://html-demo-"${TARGET_ACCOUNT_ID}"-"${PARTNER}"-"${STAGE}" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
 
 sam deploy --template-file ./templates/formation.yml \
 --stack-name html-example-"${PARTNER}"-"${STAGE}" \
