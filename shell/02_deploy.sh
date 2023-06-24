362900574d10af77e0a25911fa13ad814be56565
Abel Collins <66232145+abelcollins@users.noreply.github.com>
2023-06-24 02:17:51 -0400

    refactor: use aws cli for stack set validation

--- shell/02_deploy.sh
+++ shell/02_deploy.sh
@@ -9,7 +9,6 @@ TARGET_MODE=$5
 S3_ARTIFACTS_BUCKET="partner-services-deployment-${PARTNER}-${TARGET_ACCOUNT_ID}-${TARGET_REGION}"
 S3_ARTIFACTS_PATH="code/${SERVICE_NAME}-${PARTNER}-${STAGE}"
 
-
 echo "Printing Local scope variables";
 echo "PARTNER :: $PARTNER"
 echo "STAGE :: $STAGE"
@@ -18,7 +17,6 @@ echo "SERVICE_TYPE :: $SERVICE_TYPE"
 echo "TARGET_MODE :: $TARGET_MODE"
 echo "S3_ARTIFACTS_BUCKET :: $S3_ARTIFACTS_BUCKET"
 echo "S3_ARTIFACTS_PATH :: $S3_ARTIFACTS_PATH"
-
 
 echo "Validating the cfn templates $(date) in $(pwd)" ;
 aws cloudformation validate-template --template-body file://templates/formation.yml ;
@@ -38,6 +36,15 @@ ParameterKey=Partner,ParameterValue="${PARTNER}" \
 ParameterKey=Stage,ParameterValue="${STAGE}" \
 ParameterKey=TargetMode,ParameterValue="${TARGET_MODE}"
 
+# Check the status of cloudformation stack set
+STATUS=$(aws cloudformation describe-stacks --stack-name "${SERVICE_NAME}"-distribution-"${PARTNER}"-"${STAGE}" --output text --query "Stacks[0].StackStatus")
+if [ "$STATUS" = "CREATE_COMPLETE" ] || [ "$STATUS" = "UPDATE_COMPLETE" ]; then
+  echo "The cloudformation stack set is complete"
+else
+  echo "The cloudformation stack failed to complete"
+  exit 1
+fi
+
 echo "Retrieving hosted zone" ;
 HOSTED_ZONE=$(aws --region="us-east-1" ssm get-parameters --name "${SERVICE_NAME}-${PARTNER}-${STAGE}-hosted-zone" --output text --query "Parameters[0].Value")
 echo "Retrieving certificate" ;
@@ -53,16 +60,15 @@ ParameterKey=Partner,ParameterValue="${PARTNER}" \
 ParameterKey=Stage,ParameterValue="${STAGE}" \
 ParameterKey=TargetMode,ParameterValue="${TARGET_MODE}" \
 ParameterKey=HostedZone,ParameterValue="${HOSTED_ZONE}" \
-ParameterKey=CertificateArn,ParameterValue="${CERTIFICATE_ARN}" | tee "${SCRIPT_DIR}"/error_check_one.txt
+ParameterKey=CertificateArn,ParameterValue="${CERTIFICATE_ARN}"
 
-if grep -i -q -E "UPDATE_ROLLBACK_COMPLETE|UPDATE_ROLLBACK_FAILED|error|ValidationError|(Throttling)|failure" "${SCRIPT_DIR}"/error_check_one.txt; then
-    echo "Failed to update!"
-    exit 1
-elif [ ! -f "${SCRIPT_DIR}"/error_check_one.txt  ]; then
-    echo "Error check files are missing."
-    exit 1
+# Check the status of cloudformation stack set
+STATUS=$(aws cloudformation describe-stacks --stack-name html-example-"${PARTNER}"-"${STAGE}" --output text --query "Stacks[0].StackStatus")
+if [ "$STATUS" = "CREATE_COMPLETE" ] || [ "$STATUS" = "UPDATE_COMPLETE" ]; then
+  echo "The cloudformation stack set is complete"
 else
-    echo "No errors or failures found"
+  echo "The cloudformation stack failed to complete"
+  exit 1
 fi
 
 echo "Retrieving cloudwatch kms key" ;
