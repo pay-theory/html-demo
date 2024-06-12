683cbc34cb78a623bae023dd0fbd1f554e997555
srinithyaindla <srinithyaindla@gmail.com>
2024-06-12 15:48:36 -0400

    barcode changes

--- shell/build_env.sh
+++ shell/build_env.sh
@@ -18,6 +18,7 @@ echo "$PARTNER"-"$STAGE" Credit Card Build started on "$(date)"
 sed 's#TEMPLATE_URL#'"$SDK_URL"'#g' templates/html/pay-theory-credit-card.html | sed 's/TEMPLATE_ENVIRONMENT/'"$ENVIRONMENT"'/g' > public/"$STAGE"/"$PARTNER"/pay-theory-credit-card.html
 sed 's#TEMPLATE_URL#'"$SDK_URL"'#g' templates/html/pay-theory-credit-card-number.html | sed 's/TEMPLATE_ENVIRONMENT/'"$ENVIRONMENT"'/g' > public/"$STAGE"/"$PARTNER"/pay-theory-credit-card-number.html
 sed 's#TEMPLATE_URL#'"$SDK_URL"'#g' templates/html/pay-theory-ach.html | sed 's/TEMPLATE_ENVIRONMENT/'"$ENVIRONMENT"'/g' > public/"$STAGE"/"$PARTNER"/pay-theory-ach.html
+sed 's#TEMPLATE_URL#'"$SDK_URL"'#g' templates/html/pay-theory-barcode.html | sed 's/TEMPLATE_ENVIRONMENT/'"$ENVIRONMENT"'/g' > public/"$STAGE"/"$PARTNER"/pay-theory-barcode.html
 
 # copy in root html file
 cp index.html public/"$STAGE"/"$PARTNER"/.
